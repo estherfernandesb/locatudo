028d3f867a143d3b9d6c72e9adedcc77423ea191
Esther Bastos <100881440+estherfernandesb@users.noreply.github.com>
2024-05-03 15:33:25 -0300

    Delete Locatudo directory
    Added the new version of locatudo, in this version I worked with constructors, object manipulation and lists.

--- Locatudo/src/locatudo/model/calculator/Sortable.java
+++
@@ -1,9 +0,0 @@
-package locatudo.model.calculator;
-
-//Como não é possível criar mais de um extends,
-// então cria-se uma interface para poder atribuir notas aos filmes e séries.
-
-public interface Sortable {
-    double getClassification(); //Não precisa ser atribuído valor a interface
-
-}
